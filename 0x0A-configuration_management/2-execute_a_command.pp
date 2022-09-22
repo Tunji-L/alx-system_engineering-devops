050be01d525fc3aeed6fa5895f6c8c3cc7dcee68
Tunji-L <oyetunjilukman@gmail.com>
2022-09-22 11:12:41 +0000

    feat: Kills a process named killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # create a manifest that kills a process named killmenow
 
-exec{'pkill killmenow':
+exec {'pkill killmenow':
   command  => 'pkill killmenow'
   provider => shell
-  path     => '/usr/bin',
+  path     => '/usr/bin:/usr/sbin:/bin',
   returns  => [0, 1],
 }
